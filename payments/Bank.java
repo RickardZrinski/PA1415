054ee45b1e877f8fef767f45428bf8f72b38db42
devinant <dino.opijac@gmail.com>
2014-05-13 23:07:41 +0200

    Added the Bank class (no func. yet)

---
+++ payments/Bank.java
@@ -0,0 +1,13 @@
+package payments;
+
+/**
+ * A fake bank
+ *
+ * @author  Dino Opijac
+ * @since   13/05/14
+ */
+public class Bank {
+    public boolean authorized() {
+        return false;
+    }
+}

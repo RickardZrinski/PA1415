abd1d7ced0a83e2969051f9dff9ddf3f917ae82e
Martin Olsson <tiggetagge@gmail.com>
2014-05-14 00:19:19 +0200

    test upload of test class in IntelliJ

---
+++ game/test.java
@@ -0,0 +1,26 @@
+package game;
+
+/**
+ * Created by Martin on 2014-05-14.
+ */
+public class test
+{
+    private int testing;
+    private String meeow;
+
+    public int getTesting() {
+        return testing;
+    }
+
+    public void setTesting(int testing) {
+        this.testing = testing;
+    }
+
+    public String getMeeow() {
+        return meeow;
+    }
+
+    public void setMeeow(String meeow) {
+        this.meeow = meeow;
+    }
+}

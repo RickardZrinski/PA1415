4b6fbb3015d3f83d33e18ce14fc3117746a59721
Martin Olsson <tiggetagge@gmail.com>
2014-05-14 00:04:17 +0200

    test upload of GamesModel Basic method and attribute structure

---
+++ game/GamesModel.java
@@ -0,0 +1,26 @@
+package game;
+
+public class GamesModel
+{
+	private int nrOfGames;
+
+	public int getNrOfGames()
+	{
+		return nrOfGames;
+	}
+
+	public void addGame(String title)
+	{
+	}
+	
+	public void sendEditedParameters(int index, int nrOfThrows, int nrOfDices, WinningCondition[] winningCondition)
+	{
+		
+	}
+	
+	public GameData getGame(String index)
+	{
+		return null;
+	}
+	
+}

9b8520d2a52be554e91d2530adf94f3d32da32e4
Martin Olsson <tiggetagge@gmail.com>
2014-05-14 18:10:35 +0200

    testing for GamesModel class

---
+++ tests/GamesModel/testGamesModel.java
@@ -0,0 +1,15 @@
+package tests.GamesModel;
+
+import game.GamesModel;
+
+/**
+ * Created by Martin on 2014-05-14.
+ */
+public class testGamesModel
+{
+
+    public static void main()
+    {
+        GamesModel test;
+    }
+}

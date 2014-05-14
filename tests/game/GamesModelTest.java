b80731a1834ee96fd61c19012e63a881e0d0e5df
Martin Olsson <tiggetagge@gmail.com>
2014-05-14 19:22:21 +0200

    testing for GamesModel class

---
+++ tests/game/GamesModelTest.java
@@ -0,0 +1,41 @@
+package tests.game;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GamesModelTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetNrOfGames() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testSendEditedParameters() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetGame() throws Exception
+    {
+
+    }
+}

2eff1340c41d17c1ad26644ea790add6069194a5
Martin Olsson <tiggetagge@gmail.com>
2014-05-14 17:19:39 +0200

    completed sendEditedParameters method completed getGame method addGame method still needs work

--- game/GamesModel.java
+++ game/GamesModel.java
@@ -1,26 +1,45 @@
 package game;
 
+import java.util.ArrayList;
+
 public class GamesModel
 {
 	private int nrOfGames;
+    private ArrayList<GameData> games;
 
-	public int getNrOfGames()
+
+    public GamesModel()
+    {
+        this.nrOfGames = 0;
+    }
+
+    public int getNrOfGames()
 	{
 		return nrOfGames;
 	}
 
-	public void addGame(String title)
+    // more work needed on this method, not quite sure how to fetch game attributes from database yet.
+	public boolean addGame(String title)
 	{
+        //contact database and fetch all attributes for a new game, id, and check if inputted
+        // parameter title == gameName exists in the database
+
+        //GameData newGame = new GameData(fetched attributes)
+        //games.add(GameData newGame);
+        return true;
+	}
+
+    /*Sends Edited Parameters for a specific game*/
+	public void sendEditedParameters(int index, int nrOfThrows, int nrOfDices, ArrayList<WinningCondition> winningCondition)
+	{
+		games.get(index).setNumberOfThrows(nrOfThrows);
+        games.get(index).setNumberOfDice(nrOfDices);
+        games.get(index).setGameWinningCondition(winningCondition);
 	}
 	
-	public void sendEditedParameters(int index, int nrOfThrows, int nrOfDices, WinningCondition[] winningCondition)
+	public GameData getGame(int index)
 	{
-		
-	}
-	
-	public GameData getGame(String index)
-	{
-		return null;
+		return games.get(index);
 	}
 	
 }

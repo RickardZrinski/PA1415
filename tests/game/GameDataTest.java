647e67e79a4e73f7a346a371d6734cc2b2a65860
Martin Olsson <tiggetagge@gmail.com>
2014-05-15 23:35:46 +0200

    updated GameDataTest class

--- tests/game/GameDataTest.java
+++ tests/game/GameDataTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class GameDataTest
 {
-    public static void main()
+    public static void main(String[] args)
     {
         Dapper<GameData> sql = new Dapper<>(GameData.class);
 

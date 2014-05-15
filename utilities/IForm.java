2fe519b839728a18e85a15b5e1e39d273fe4f4c6
devinant <dino.opijac@gmail.com>
2014-05-15 04:18:38 +0200

    Interface required to implement for utility.Form to work

---
+++ utilities/IForm.java
@@ -0,0 +1,10 @@
+package utilities;
+
+/**
+ * All Views with that should be translated to objects must implement
+ * this view. Used in conjunction with {@link utilities.Form}
+ *
+ * @author  Dino Opijac
+ * @since   14/05/14
+ */
+public interface IForm {}

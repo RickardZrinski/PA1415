cd488270d4f46d040afb30b77923290271f6843f
Martin Olsson <tiggetagge@gmail.com>
2014-05-15 22:50:31 +0200

    basic structure of ListAllGamesController, needs more work

---
+++ controllers/ListAllGamesController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Martin on 2014-05-15.
+ */
+public class ListAllGamesController extends Controller
+{
+    private ArrayList<Controller> controller;
+
+    public ListAllGamesController() //MainFrame object as parameter?
+    {
+
+    }
+}

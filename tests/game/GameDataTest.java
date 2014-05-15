96329a6fc4511fc517de2e46a64cf6f57a0d024c
Martin Olsson <tiggetagge@gmail.com>
2014-05-15 23:12:36 +0200

    updated GameDataTest class

---
+++ tests/game/GameDataTest.java
@@ -0,0 +1,19 @@
+package tests.game;
+
+import game.GameData;
+import sql.Dapper;
+
+import static org.junit.Assert.*;
+
+public class GameDataTest
+{
+    public static void main()
+    {
+        Dapper<GameData> sql = new Dapper<>(GameData.class);
+
+        for(GameData data : sql.getCollection())
+        {
+            System.out.println(data);
+        }
+    }
+}

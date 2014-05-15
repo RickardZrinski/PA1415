4a0ac9e9f6e2db9e66212da670628a60f5128928
Martin Olsson <tiggetagge@gmail.com>
2014-05-15 22:51:52 +0200

    basic structure of super class Controller that other controller classes inherits from. For the admin GUI part

---
+++ controllers/Controller.java
@@ -0,0 +1,15 @@
+package controllers;
+
+/**
+ * Created by Martin on 2014-05-15.
+ */
+public class Controller
+{
+
+    public Controller()// MainFrame object som parameter?
+    {
+
+    }
+
+
+}

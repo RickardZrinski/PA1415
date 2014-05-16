14a283605c16a3abb45697b42650d53f28728aea
devinant <dino.opijac@gmail.com>
2014-05-16 19:58:26 +0200

    Removed setId and id from constructor.

--- game/GameData.java
+++ game/GameData.java
@@ -38,8 +38,7 @@ public class GameData {
      * @param numberOfThrows    number of throws done by player
      * @param rules rules of the game
      */
-    public GameData(int id, String gameName, int numberOfDice, int numberOfThrows, String rules) {
-        this.id = id;
+    public GameData(String gameName, int numberOfDice, int numberOfThrows, String rules) {
         this.gameName = gameName;
         this.numberOfThrows = numberOfThrows;
         this.numberOfDice = numberOfDice;
@@ -141,10 +140,6 @@ public class GameData {
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getGameName() {

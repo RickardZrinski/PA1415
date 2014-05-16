28489c44c9ec618dc2bfe6419578e12e3ea1aedd
Rickard <ricco3@hotmail.com>
2014-05-16 22:04:02 +0200

    Add classes ListAllUsersView and ListAllUsersController

---
+++ administrator/controllers/ListAllUsersController.java
@@ -0,0 +1,18 @@
+package administrator.controllers;
+
+import administrator.GUI;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rickard Zrinski on 2014-05-15.
+ */
+public class ListAllUsersController extends Controller
+{
+    private ArrayList<Controller> controller;
+
+    public ListAllUsersController(GUI gui)
+    {
+        super(gui);
+    }
+}

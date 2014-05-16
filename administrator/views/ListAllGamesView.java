6a063fcb54480693f8d633b8dc7a3d7a7c8dd314
Rickard <ricco3@hotmail.com>
2014-05-16 22:02:09 +0200

    Add classes View and ListAllGamesView

---
+++ administrator/views/ListAllGamesView.java
@@ -0,0 +1,14 @@
+package administrator.views;
+
+import javax.swing.*;
+
+/**
+ * Created by Rickard Zrinski on 2014-05-16.
+ */
+public class ListAllGamesView extends View
+{
+    public ListAllGamesView()
+    {
+        this.add(new JButton("List all games!"));
+    }
+}

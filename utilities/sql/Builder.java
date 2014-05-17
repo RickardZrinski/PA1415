17749e36d3bac4c79e430ff84b3dada3733ac038
devinant <dino.opijac@gmail.com>
2014-05-17 21:48:51 +0200

    Added Builder.comma and Builder.and

--- utilities/sql/Builder.java
+++ utilities/sql/Builder.java
@@ -7,15 +7,15 @@ import java.util.*;
  * @since   17/05/2014
  */
 public class Builder {
-
     /**
-     * Generates a custom delete String. Every odd argument represents the column name, every
+     * Generates a custom equals string. Every odd argument represents the column name, every
      * even argument sets the value.
      *
+     * @param   separator   the separator for each column/value pair
      * @param   objects     arguments to add to the query
      * @return              a string of the query
      */
-    public static String equals(Object... objects) {
+    public static String equals(String separator, Object... objects) {
         int num = 0;
 
         StringBuilder builder = new StringBuilder();
@@ -24,10 +24,30 @@ public class Builder {
             if ((num++ & 1) == 0)
                 builder.append(String.format("%s = ", arg));
             else
-                builder.append("? AND ");
+                builder.append(String.format("? %s ", separator));
         }
 
         return builder.substring(0, builder.toString().length() - 5) + ";";
+    }
+
+    /**
+     * Generates a custom equals string with AND as separator.
+     * Often used with DELETE keyword
+     * @param   objects     arguments to add to the query
+     * @return              a string of the query
+     */
+    public static String and(Object... objects) {
+        return Builder.equals(" AND ", objects);
+    }
+
+    /**
+     * Generates a custom equals string with a comma as separator.
+     * Often used with UPDATE or SET keyword(s)
+     * @param   objects     arguments to add to the query
+     * @return              a string of the query
+     */
+    public static String comma(Object... objects) {
+        return Builder.equals(", ", objects);
     }
 
     /**

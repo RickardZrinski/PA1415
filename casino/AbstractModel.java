9f95fb9ccf52f0ca51502ce0f4399af26067b196
devinant <dino.opijac@gmail.com>
2014-05-19 02:12:49 +0200

    Added AbstractModel

---
+++ casino/AbstractModel.java
@@ -0,0 +1,9 @@
+package casino;
+
+/**
+ * @author  Dino Opijac
+ * @since   18/05/14
+ */
+public abstract class AbstractModel {
+    // @TODO: Decide if the model should talk to the view (or be decoupled by a interface, perhaps?)
+}

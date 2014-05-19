438e0787e132ef48de9f00867fb6ed9112eddaa9
devinant <dino.opijac@gmail.com>
2014-05-19 02:30:39 +0200

    Now using GridBagUtilities instead of private methods

--- casino/views/form/LoginForm.java
+++ casino/views/form/LoginForm.java
@@ -1,5 +1,8 @@
 package casino.views.form;
 
+import utilities.ComponentUtilities;
+import utilities.GridBagUtilities;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -22,45 +25,36 @@ public class LoginForm extends JPanel {
 
     private void configure()
     {
-        Dimension size = this.usernameTextField.getPreferredSize();
-        size.setSize(300, size.getHeight());
+        ComponentUtilities.setWidth(50, this.usernameTextField);
+        ComponentUtilities.setWidth(50, this.passwordPasswordField);
 
-            this.usernameTextField.setPreferredSize(size);
-        this.passwordPasswordField.setPreferredSize(size);
-    }
-
-    private GridBagConstraints fill(JComponent parent, JComponent component, int w, int x, int y, int constraint, int anchor) {
-        return this.fill(parent, component, w, x, y, constraint, anchor, new GridBagConstraints());
-    }
-
-    private GridBagConstraints fill(JComponent parent, JComponent component, int w, int x, int y, int constraint, int anchor, GridBagConstraints constraints) {
-        constraints.fill = constraint;
-        constraints.gridwidth = w;
-        constraints.gridx = x;
-        constraints.gridy = y;
-        constraints.anchor = anchor;
-
-        parent.add(component, constraints);
-
-        return constraints;
+        this.signInButton.setActionCommand("login");
+        this.resetButton.setActionCommand("reset");
     }
 
     private void addComponents() {
         this.setLayout(new GridBagLayout());
 
-        this.fill(this, new JLabel("Username", JLabel.TRAILING), 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.EAST);
-        this.fill(this, this.usernameTextField, 0, 1, 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+        JLabel usernameLabel = new JLabel("Username");
+        JLabel passwordLabel = new JLabel("Password");
 
-        this.fill(this, new JLabel("Password", JLabel.TRAILING), 1, 0, 1, GridBagConstraints.LINE_START, GridBagConstraints.EAST);
-        this.fill(this, this.passwordPasswordField, 0, 1, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+        ComponentUtilities.setWidth(100, usernameLabel);
+        ComponentUtilities.setWidth(100, passwordLabel);
 
-        GridBagConstraints previous = this.fill(this, this.signInButton, 2, 1, 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
-                                      this.fill(this, this.resetButton,  2, 4, 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST, previous);
+        GridBagUtilities.makeCell(this, usernameLabel, new Point(0, 0), 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.usernameTextField, new Point(2, 0), 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, passwordLabel, new Point(0, 1), 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.passwordPasswordField, new Point(2, 1), 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, this.signInButton, new Point(2, 2), 2);
+        GridBagUtilities.makeCell(this, this.resetButton, new Point(4, 2), 2);
     }
 
     public JTextField getUsernameTextField() {
         return this.usernameTextField;
     }
+
     public JPasswordField getPasswordPasswordField() {
         return this.passwordPasswordField;
     }

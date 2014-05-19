0fd4740a4cc3c76314da98ac14c7824c3e47586e
devinant <dino.opijac@gmail.com>
2014-05-19 02:13:27 +0200

    Added Main for casino

---
+++ casino/Main.java
@@ -0,0 +1,14 @@
+package casino;
+
+import casino.controllers.Controller;
+
+/**
+ * @author  Dino Opijac
+ * @since   18/05/2014
+ */
+public class Main {
+    public static void main(String[] args) {
+        // @TODO: Add model behavior (see views)
+        new Controller();
+    }
+}

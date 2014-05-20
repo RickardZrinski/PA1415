3fe0ea878e35283565f858dc1ad19ab428e5dada
devinant <dino.opijac@gmail.com>
2014-05-20 15:27:28 +0200

    Added setPreferredHeight and setPreferredWidth

--- utilities/ComponentUtilities.java
+++ utilities/ComponentUtilities.java
@@ -23,9 +23,9 @@ public class ComponentUtilities {
      * @param width     the new width
      * @param component the component to operate on
      */
-    public static void setWidth(int width, Component component) {
+    public static void setPreferredWidth(int width, Component component) {
         Dimension size = component.getPreferredSize();
-        component.setSize(width, (int)size.getHeight());
+        component.setPreferredSize(new Dimension(width, (int) size.getHeight()));
     }
 
     /**
@@ -33,8 +33,28 @@ public class ComponentUtilities {
      * @param height    the new width
      * @param component the component to operate on
      */
-    public static void setHeight(int height, Component component) {
+    public static void setPreferredHeight(int height, Component component) {
         Dimension size = component.getPreferredSize();
+        component.setPreferredSize(new Dimension((int) size.getWidth(), height));
+    }
+
+    /**
+     * Changes only the width of the component, leaves the height to the size.
+     * @param width     the new width
+     * @param component the component to operate on
+     */
+    public static void setWidth(int width, Component component) {
+        Dimension size = component.getSize();
+        component.setSize(width, (int)size.getHeight());
+    }
+
+    /**
+     * Changes only the height of the component, leaves the width to the size.
+     * @param height    the new width
+     * @param component the component to operate on
+     */
+    public static void setHeight(int height, Component component) {
+        Dimension size = component.getSize();
         component.setSize((int)size.getWidth(), height);
     }
 }

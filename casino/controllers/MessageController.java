5a099263ed0dfb5a9c316e5340c1ce21fe6b4f24
JamJaws <jamjaws@gmail.com>
2014-05-20 21:34:37 +0200

    MessageController implements MessageListener.

--- casino/controllers/MessageController.java
+++ casino/controllers/MessageController.java
@@ -1,6 +1,8 @@
 package casino.controllers;
 
 import casino.MainFrame;
+import casino.events.LoginEvent;
+import casino.events.MessageListener;
 import casino.views.MessageView;
 
 import java.awt.*;
@@ -9,12 +11,19 @@ import java.awt.*;
  * @author  John Mogensen
  * @since   20/05/2014
  */
-public class MessageController {
+public class MessageController implements MessageListener {
     private MessageView messageView = new MessageView();
 
     public MessageController() {
         this.messageView.subscribe(this);
 
         MainFrame.getInstance().add(this.messageView, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void messageSend(LoginEvent e) {
+        if (e.isValid()){
+            System.out.println("MESSAGE INPUT VALID");
+        }
     }
 }

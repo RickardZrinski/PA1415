a2e3b85dd1af017da34c2a7c000294aa2a408f8d
JamJaws <jamjaws@gmail.com>
2014-05-20 16:38:22 +0200

    MessageForm draft.

--- casino/views/forms/MessageForm.java
+++ casino/views/forms/MessageForm.java
@@ -1,11 +1,105 @@
 package casino.views.forms;
 
+import utilities.ComponentUtilities;
+import utilities.GridBagUtilities;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * @author  Dino Opijac
  * @since   19/05/2014
  */
 public class MessageForm extends JPanel {
+
+    private JTextField nameTextField;
+    private JTextField subjectTextField;
+    private JComboBox categoryComboBox;
+    private JTextField contactDetailsTextField;
+    private JTextArea messageTextArea;
+    private JButton nextButton;
+    private JButton cancelButton;
+
+    public MessageForm() {
+        // Form elements
+        this.nameTextField = new JTextField();
+        this.subjectTextField = new JTextField();
+        this.categoryComboBox  = new JComboBox<String>(new String[] {"Support", "Praise", "Feature request"});
+        this.contactDetailsTextField = new JTextField();
+        this.messageTextArea = new JTextArea();
+        this.nextButton = new JButton("Next");
+        this.cancelButton = new JButton("Cancel");
+        this.configure();
+        this.addComponents();
+    }
+
+    private void configure()
+    {
+        this.nextButton.setActionCommand("next");
+        this.cancelButton.setActionCommand("cancel");
+    }
+
+    private void addComponents() {
+        this.setLayout(new GridBagLayout());
+
+        JLabel nameLabel = new JLabel("Full Name");
+        JLabel subjectLabel = new JLabel("Subject");
+        JLabel categoryLabel = new JLabel("Category");
+        JLabel contactDetailsLabel = new JLabel("Contact Details");
+        JLabel messageLabel = new JLabel("Message");
+        JTextArea contactDetailsInfoTextArea = new JTextArea("Where can we reach you? Leave a phone number or email address.");
+        contactDetailsInfoTextArea.setEditable(false);
+
+        JPanel buttons = new JPanel();
+        buttons.add(this.nextButton);
+        buttons.add(this.cancelButton);
+
+
+        GridBagUtilities.makeCell(this, nameLabel,              new Point(0, 0), 0.25, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.nameTextField,     new Point(1, 0), 0.75, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, subjectLabel,           new Point(0, 1), 0.25, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.subjectTextField,  new Point(1, 1), 0.27, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, categoryLabel,          new Point(0, 2), 0.25, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.categoryComboBox,  new Point(1, 2), 0.50, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, contactDetailsLabel,            new Point(0, 3), 0.25, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.contactDetailsTextField,   new Point(1, 3), 0.75, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, this.contactDetailsTextField,   new Point(1, 4), 1.0, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, messageLabel,           new Point(0, 5), 0.25, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, this.messageTextArea,   new Point(1, 5), 0.75, 1, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+
+
+        GridBagUtilities.makeCell(this, buttons,                new Point(1, 6), 0.75, 1, GridBagConstraints.NONE, GridBagConstraints.WEST);
+    }
+
+    public JTextField getNameTextField() {
+        return this.nameTextField;
+    }
+
+    public JTextField getSubjectTextField() {
+        return this.subjectTextField;
+    }
+
+    public JComboBox getCategoryComboBox() {
+        return this.categoryComboBox;
+    }
+
+    public JTextField getContactDetailsTextField() {
+        return this.contactDetailsTextField;
+    }
+
+    public JTextArea getMessageTextArea() {
+        return this.messageTextArea;
+    }
+
+    public void setActionListener(ActionListener listener) {
+        this.nextButton.addActionListener(listener);
+        this.cancelButton.addActionListener(listener);
+    }
 
 }

9aab6dd2d4f10b26797b8cfb6466435abd0b400a
devinant <dino.opijac@gmail.com>
2014-05-20 22:44:29 +0200

    Implemented mutators, accessors and a default constructor

--- shared/messages/relation/MessageCategories.java
+++ shared/messages/relation/MessageCategories.java
@@ -9,4 +9,53 @@ import utilities.sql.annotations.PrimaryKey;
 public class MessageCategories {
     @PrimaryKey private int mid;
     private int cid;
+
+    /**
+     * Creates the standard message category
+     */
+    public MessageCategories() {
+        this.mid = 1;
+        this.cid = 1;
+    }
+
+    /**
+     * Sets the relation between two objects using the mid and cid
+     * @param mid the message id
+     * @param cid the category id
+     */
+    public MessageCategories(int mid, int cid) {
+        this.mid = mid;
+        this.cid = cid;
+    }
+
+    /**
+     * Sets the category id
+     * @param cid the category id
+     */
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    /**
+     * Sets the message id
+     * @param mid the message id
+     */
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    /**
+     * @return the category id
+     */
+    public int getCid() {
+        return this.cid;
+    }
+
+    /**
+     * @return the message id
+     */
+    public int getMid() {
+        return this.mid;
+    }
+
 }

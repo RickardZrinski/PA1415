7bf08c79e8069a35e6ad611c1456f1881bdf7a53
devinant <dino.opijac@gmail.com>
2014-05-20 19:09:14 +0200

    Introduced the Form interface for all subviews handling forms

---
+++ casino/views/forms/Form.java
@@ -0,0 +1,11 @@
+package casino.views.forms;
+
+import java.awt.event.ActionListener;
+
+/**
+ * @author  Dino Opijac
+ * @since   20/05/2014
+ */
+public interface Form {
+    public void setActionListener(ActionListener listener);
+}

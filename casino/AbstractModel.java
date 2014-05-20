885f7435ce3706ac8610ace47936f34860dee0f1
devinant <dino.opijac@gmail.com>
2014-05-20 03:33:44 +0200

    AbstractModel remade. Now acts as a observable.

--- casino/AbstractModel.java
+++ casino/AbstractModel.java
@@ -1,9 +1,44 @@
 package casino;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 /**
  * @author  Dino Opijac
  * @since   18/05/14
  */
 public abstract class AbstractModel {
-    // @TODO: Decide if the model should talk to the view (or be decoupled by a interface, perhaps?)
+    private ArrayList<Object> observers = new ArrayList<>();
+    /**
+     * Subscribes a new subject to the observer
+     * @param subject the subject that wishes to subscribe
+     */
+    public void subscribe(Object subject) {
+        this.observers.add(subject);
+    }
+
+    /**
+     * Unsubscribes a new subject from the observer
+     * @param subject the subject that wishes to unsubscribe
+     */
+    public void unsubscribe(Object subject) {
+        this.observers.remove(subject);
+    }
+
+    /**
+     * Invokes a method inside a list of observers
+     * @param name  the name of the event
+     * @param event the event itself
+     */
+    public void notify(Object name, Object event) {
+        for (Object listener: this.observers) {
+            try {
+                // Call the method with the event
+                Method method = listener.getClass().getMethod((String) name, new Class[]{event.getClass()});
+                method.invoke(listener, event);
+            } catch (Exception ex) {
+                System.out.println(String.format("notify: %s not found in %s. Attempted to send:\n%s", name, listener.getClass().getName(), event));
+            }
+        }
+    }
 }

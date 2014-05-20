7c7df1f6f9d0395733ef2914c750c0d7e9f74e1e
devinant <dino.opijac@gmail.com>
2014-05-20 15:26:56 +0200

    Described how the GridBagLayout worked

--- casino/views/forms/LoginForm.java
+++ casino/views/forms/LoginForm.java
@@ -25,9 +25,6 @@ public class LoginForm extends JPanel {
 
     private void configure()
     {
-        ComponentUtilities.setWidth(50, this.usernameTextField);
-        ComponentUtilities.setWidth(50, this.passwordPasswordField);
-
         this.signInButton.setActionCommand("login");
         this.resetButton.setActionCommand("reset");
     }
@@ -38,17 +35,36 @@ public class LoginForm extends JPanel {
         JLabel usernameLabel = new JLabel("Username");
         JLabel passwordLabel = new JLabel("Password");
 
-        ComponentUtilities.setWidth(100, usernameLabel);
-        ComponentUtilities.setWidth(100, passwordLabel);
+        // Create a JPanel for the two grouped buttons
+        JPanel buttons = new JPanel();
+        buttons.add(this.signInButton);
+        buttons.add(this.resetButton);
 
-        GridBagUtilities.makeCell(this, usernameLabel, new Point(0, 0), 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
-        GridBagUtilities.makeCell(this, this.usernameTextField, new Point(2, 0), 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+        /**
+         * GridBagLayout works with grids. The point is a coordinate inside a table, for example:
+         *  (x,y)
+         * +-----+-----+-----+
+         * |(0,0)|(1,0)|(2,0)|
+         * +-----+-----+-----+
+         * |(0,1)|(1,1)|(2,1)|
+         * +-----+-----+-----+
+         *
+         * The percentage (labeled % below) is the width of the column. Columns in the above table should have 0.33 (since 100% table / 3 columns = 33%)
+         * The width is how many positions a column should occupy. In the example below, the second row occupies 3 columns and has Point (0, 1)
+         * *  (x,y)
+         * +-----+-----+-----+
+         * |(0,0)|(1,0)|(2,0)|
+         * +-----+-----+-----+
+         * |(0,1)            | (1,1) and (2,1) are replaced entirely with (0,1), since (0,1) has width 3
+         * +-----+-----+-----+
+         */
 
-        GridBagUtilities.makeCell(this, passwordLabel, new Point(0, 1), 2, GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
-        GridBagUtilities.makeCell(this, this.passwordPasswordField, new Point(2, 1), 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
-
-        GridBagUtilities.makeCell(this, this.signInButton, new Point(2, 2), 2);
-        GridBagUtilities.makeCell(this, this.resetButton, new Point(4, 2), 2);
+        //                                                                    x, y   %     width
+        GridBagUtilities.makeCell(this, usernameLabel,              new Point(0, 0), 0.25, 1,    GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+        GridBagUtilities.makeCell(this, this.usernameTextField,     new Point(1, 0), 0.75, 1,    GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, passwordLabel,              new Point(0, 1), 0.25, 1,    GridBagConstraints.HORIZONTAL, GridBagConstraints.WEST);
+        GridBagUtilities.makeCell(this, this.passwordPasswordField, new Point(1, 1), 0.75, 1,    GridBagConstraints.HORIZONTAL, GridBagConstraints.EAST);
+        GridBagUtilities.makeCell(this, buttons,                    new Point(1, 2), 0.75, 1,    GridBagConstraints.NONE, GridBagConstraints.WEST);
     }
 
     public JTextField getUsernameTextField() {

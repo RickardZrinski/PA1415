33e74ecfe19ef87274566ffce486132f09cda388
devinant <dino.opijac@gmail.com>
2014-05-21 18:38:19 +0200

    Added mutators

--- shared/users/Role.java
+++ shared/users/Role.java
@@ -49,8 +49,10 @@ public class Role {
         this.canWithdraw = true;
     }
 
-    public int getId() { return this.id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return this.id;
+    }
+
     public String getName() {
         return this.name;
     }
@@ -58,6 +60,7 @@ public class Role {
     public boolean canAccessAdministration() {
         return this.canAccessAdministration;
     }
+
     public boolean canAuthenticate() {
         return this.canAuthenticate;
     }
@@ -65,6 +68,7 @@ public class Role {
     public boolean canSendMessages() {
         return this.canSendMessages;
     }
+
     public boolean canReadMessages() {
         return this.canReadMessages;
     }
@@ -72,6 +76,7 @@ public class Role {
     public boolean canSwitchLanguage() {
         return this.canSwitchLanguage;
     }
+
     public boolean canSetDailyLossLimit() {
         return this.canSetDailyLossLimit;
     }
@@ -79,12 +84,15 @@ public class Role {
     public boolean canListAllGames() {
         return this.canListAllGames;
     }
+
     public boolean canInsertGames() {
         return this.canInsertGames;
     }
+
     public boolean canUpdateGames() {
         return this.canUpdateGames;
     }
+
     public boolean canDeleteGames() {
         return this.canDeleteGames;
     }
@@ -92,15 +100,19 @@ public class Role {
     public boolean canListAllUsers() {
         return this.canListAllUsers;
     }
+
     public boolean canInsertUsers() {
         return this.canInsertUsers;
     }
+
     public boolean canUpdateUsers() {
         return this.canUpdateUsers;
     }
+
     public boolean canDeleteUsers() {
         return this.canDeleteUsers;
     }
+
     public boolean canBanUsers() {
         return this.canBanUsers;
     }
@@ -108,8 +120,85 @@ public class Role {
     public boolean canDeposit() {
         return this.canDeposit;
     }
+
     public boolean canWithdraw() {
         return this.canWithdraw;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCanAccessAdministration(boolean canAccessAdministration) {
+        this.canAccessAdministration = canAccessAdministration;
+    }
+
+    public void setCanAuthenticate(boolean canAuthenticate) {
+        this.canAuthenticate = canAuthenticate;
+    }
+
+    public void setCanSendMessages(boolean canSendMessages) {
+        this.canSendMessages = canSendMessages;
+    }
+
+    public void setCanReadMessages(boolean canReadMessages) {
+        this.canReadMessages = canReadMessages;
+    }
+
+    public void setCanSwitchLanguage(boolean canSwitchLanguage) {
+        this.canSwitchLanguage = canSwitchLanguage;
+    }
+
+    public void setCanSetDailyLossLimit(boolean canSetDailyLossLimit) {
+        this.canSetDailyLossLimit = canSetDailyLossLimit;
+    }
+
+    public void setCanListAllGames(boolean canListAllGames) {
+        this.canListAllGames = canListAllGames;
+    }
+
+    public void setCanInsertGames(boolean canInsertGames) {
+        this.canInsertGames = canInsertGames;
+    }
+
+    public void setCanUpdateGames(boolean canUpdateGames) {
+        this.canUpdateGames = canUpdateGames;
+    }
+
+    public void setCanDeleteGames(boolean canDeleteGames) {
+        this.canDeleteGames = canDeleteGames;
+    }
+
+    public void setCanListAllUsers(boolean canListAllUsers) {
+        this.canListAllUsers = canListAllUsers;
+    }
+
+    public void setCanInsertUsers(boolean canInsertUsers) {
+        this.canInsertUsers = canInsertUsers;
+    }
+
+    public void setCanUpdateUsers(boolean canUpdateUsers) {
+        this.canUpdateUsers = canUpdateUsers;
+    }
+
+    public void setCanDeleteUsers(boolean canDeleteUsers) {
+        this.canDeleteUsers = canDeleteUsers;
+    }
+
+    public void setCanBanUsers(boolean canBanUsers) {
+        this.canBanUsers = canBanUsers;
+    }
+
+    public void setCanDeposit(boolean canDeposit) {
+        this.canDeposit = canDeposit;
+    }
+
+    public void setCanWithdraw(boolean canWithdraw) {
+        this.canWithdraw = canWithdraw;
     }
 
     @Override

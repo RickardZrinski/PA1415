23c6fd4213eec885be48a48295927f9af26f964d
devinant <dino.opijac@gmail.com>
2014-05-21 19:13:28 +0200

    Added a plain generator

--- utilities/sql/Builder.java
+++ utilities/sql/Builder.java
@@ -54,6 +54,48 @@ public class Builder {
     }
 
     /**
+     * Generates a plain string with separated values, has the same behaviour as linear.
+     *
+     * @param   separator   the separator for each column
+     * @param   objects     objects to add as column names
+     * @return              a partial string for the query
+     */
+    public static String plain(String separator, Object... objects) {
+        StringBuilder builder = new StringBuilder();
+
+        for(Object o: objects) {
+            if (o instanceof Field)
+                builder.append(((Field) o).getName());
+            else
+                builder.append(o);
+
+            builder.append(separator);
+        }
+
+        return builder.substring(0, builder.toString().length() - separator.length());
+    }
+
+    /**
+     * Returns a filled string with values repeated {@param amount} times separated
+     * by {@param separator}.
+     *
+     * @param   filler      the filler to fill with
+     * @param   separator   the separator to separate filler with
+     * @param   amount      the amount of times for this should be repeated
+     * @return              a filled string separated by separator amount times
+     */
+    public static String fill(String filler, String separator, int amount) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < amount; i++) {
+            builder.append(filler);
+            builder.append(separator);
+        }
+
+        return builder.substring(0, builder.toString().length() - separator.length());
+    }
+
+    /**
      * Generates a custom equals string with AND as separator.
      * Often used with DELETE keyword
      * @param   objects     arguments to add to the query

f9275920e15fcb01ec6e5979d1b42a8d2c616947
devinant <dino.opijac@gmail.com>
2014-05-21 16:17:04 +0200

    Fully functional login system

--- casino/models/LoginModel.java
+++ casino/models/LoginModel.java
@@ -1,18 +1,40 @@
 package casino.models;
 import casino.AbstractModel;
-
-import java.util.Random;
+import casino.events.LoginEvent;
+import shared.AuthenticationSession;
+import shared.dao.UserDao;
+import shared.users.User;
+import utilities.Hash;
 
 public class LoginModel extends AbstractModel {
+    private UserDao users = new UserDao();
 
     public LoginModel() {}
 
-    // Currently does nothing
-    public void doSomething() {
-        System.out.println("LoginModel#doSomething() invoked.");
+    public void authenticate(LoginEvent event) {
+        try {
+            // Attempt to retrieve the user
+            User user = users.get(event.getUsername());
+            System.out.println(user);
+            System.out.println(Hash.validatePassword(event.getPassword(), user.getPassword()));
 
-        // Generate a random and notify the observers
-        Random random = new Random();
-        this.notify("loginResponse", random.nextBoolean());
+            /**
+             * Check if the user is allowed to sign in; his or her role will let us know.
+             * Compare the password retrieved and the password stored in the database.
+             */
+            if (user.getRole().canAuthenticate() && Hash.validatePassword(event.getPassword(), user.getPassword())) {
+                // Get a instance of the authentication and start it
+                AuthenticationSession.start(user);
+
+                // Let our listeners know that sign in was a success.
+                this.notify("loginSuccessful");
+
+            } else
+                this.notify("loginUnsuccessful"); // The password is wrong or the user cannot log in
+
+        } catch (Exception e) {
+            this.notify("loginUnsuccessful");
+            e.printStackTrace();
+        }
     }
 }

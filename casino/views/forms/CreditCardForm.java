835b774f9ceee1e19893dc28aabf4679d95840fc
devinant <dino.opijac@gmail.com>
2014-05-22 23:37:40 +0200

    Handles own events and enables the use of creditCardCancel, creditCardAction and creditCardError

--- casino/views/forms/CreditCardForm.java
+++ casino/views/forms/CreditCardForm.java
@@ -1,5 +1,7 @@
 package casino.views.forms;
 
+import casino.AbstractView;
+import shared.transactions.payments.CreditCard;
 import utilities.ComponentUtilities;
 import utilities.GridBagUtilities;
 
@@ -7,6 +9,7 @@ import javax.swing.*;
 import javax.swing.text.MaskFormatter;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 import java.util.Formatter;
@@ -15,7 +18,9 @@ import java.util.Formatter;
  * @author  Dino Opijac
  * @since   21/05/2014
  */
-public class CreditCardForm extends JPanel implements Form {
+public class CreditCardForm extends AbstractView implements ActionListener {
+    private JLabel resultLabel = new JLabel();
+    private JTextField resultTextField = new JTextField();
     private JTextField holderTextField = new JTextField();
     private JFormattedTextField numberTextField = new JFormattedTextField(ComponentUtilities.createMaskFormat("#### #### #### ####"));
     private JFormattedTextField securityCodeTextField = new JFormattedTextField(ComponentUtilities.createMaskFormat("###"));
@@ -26,15 +31,26 @@ public class CreditCardForm extends JPanel implements Form {
     private JButton nextButton;
     private JButton cancelButton;
 
-    public CreditCardForm() {
+    public CreditCardForm(String text) {
         this.expirationMonthBox = new JComboBox<>();
         this.expirationYearBox = new JComboBox<>();
 
         this.nextButton = new JButton("Next");
         this.cancelButton = new JButton("Cancel");
 
+        if (text == null)
+            this.hideResultRow();
+        else {
+            this.resultLabel.setText(text);
+            this.resultTextField.setEnabled(false);
+        }
+
         this.configure();
         this.addComponents();
+    }
+
+    public CreditCardForm() {
+        this(null);
     }
 
     private void configure() {
@@ -45,6 +61,12 @@ public class CreditCardForm extends JPanel implements Form {
 
         ComponentUtilities.setWidth(200, this.securityCodeTextField);
         ComponentUtilities.setPreferredWidth(40, this.securityCodeTextField);
+
+        // The next button should listen to this class, it emits a creditCardAction event
+        this.nextButton.addActionListener(this);
+        this.cancelButton.addActionListener(this);
+
+        this.cancelButton.setActionCommand("cancel");
     }
 
     private void addComponents() {
@@ -58,19 +80,30 @@ public class CreditCardForm extends JPanel implements Form {
         JPanel buttons = ComponentUtilities.flow(FlowLayout.LEFT, this.nextButton, this.cancelButton);
         JPanel boxes = ComponentUtilities.flow(FlowLayout.LEFT, this.expirationMonthBox, this.expirationYearBox);
 
-        GridBagUtilities.makeCell(this, holderLabel,                   new Point(0, 0), 0.2, 1, GridBagConstraints.HORIZONTAL);
-        GridBagUtilities.makeCell(this, this.holderTextField,          new Point(1, 0), 0.8, 4, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, this.resultLabel,              new Point(0, 0), 0.2, 1, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, this.resultTextField,          new Point(1, 0), 0.8, 4, GridBagConstraints.HORIZONTAL);
 
-        GridBagUtilities.makeCell(this, numberLabel,                   new Point(0, 1), 0.2, 1, GridBagConstraints.HORIZONTAL);
-        GridBagUtilities.makeCell(this, this.numberTextField,          new Point(1, 1), 0.8, 4, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, holderLabel,                   new Point(0, 1), 0.2, 1, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, this.holderTextField,          new Point(1, 1), 0.8, 4, GridBagConstraints.HORIZONTAL);
 
-        GridBagUtilities.makeCell(this, expirationLabel,               new Point(0, 2), 0.2, 1, GridBagConstraints.HORIZONTAL);
-        GridBagUtilities.makeCell(this, boxes,                         new Point(1, 2), 0.8, 4, GridBagConstraints.NONE, GridBagConstraints.WEST);
+        GridBagUtilities.makeCell(this, numberLabel,                   new Point(0, 2), 0.2, 1, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, this.numberTextField,          new Point(1, 2), 0.8, 4, GridBagConstraints.HORIZONTAL);
 
-        GridBagUtilities.makeCell(this, securityCodeLabel,             new Point(0, 3), 0.2, 1, GridBagConstraints.HORIZONTAL);
-        GridBagUtilities.makeCell(this, this.securityCodeTextField,    new Point(1, 3), 0.8, 1, GridBagConstraints.RELATIVE, GridBagConstraints.WEST);
+        GridBagUtilities.makeCell(this, expirationLabel,               new Point(0, 3), 0.2, 1, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, boxes,                         new Point(1, 3), 0.8, 4, GridBagConstraints.NONE, GridBagConstraints.WEST);
 
-        GridBagUtilities.makeCell(this, buttons,                       new Point(1, 4), 1, 4, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, securityCodeLabel,             new Point(0, 4), 0.2, 1, GridBagConstraints.HORIZONTAL);
+        GridBagUtilities.makeCell(this, this.securityCodeTextField,    new Point(1, 4), 0.8, 1, GridBagConstraints.RELATIVE, GridBagConstraints.WEST);
+
+        GridBagUtilities.makeCell(this, buttons,                       new Point(1, 5), 1, 4, GridBagConstraints.HORIZONTAL);
+    }
+
+    public JLabel getResultLabel() {
+        return this.resultLabel;
+    }
+
+    public JTextField getResultTextField() {
+        return this.resultTextField;
     }
 
     public JTextField getHolderTextField() {
@@ -101,15 +134,40 @@ public class CreditCardForm extends JPanel implements Form {
         return this.cancelButton;
     }
 
-    @Override
-    public void setActionListener(ActionListener listener) {
-        this.nextButton.addActionListener(listener);
-        this.cancelButton.addActionListener(listener);
+    public void hideResultRow() {
+        this.resultLabel.setVisible(false);
+        this.resultTextField.setVisible(false);
+    }
+
+    public void showResultRow() {
+        this.resultLabel.setVisible(true);
+        this.resultTextField.setVisible(true);
     }
 
     @Override
     public String toString() {
         return String.format("holder: %s, number: %s, expiration: %s/%s, code: %s", this.holderTextField.getText(), this.numberTextField.getText(),
                 this.expirationMonthBox.getSelectedItem(), this.expirationYearBox.getSelectedItem(), this.securityCodeTextField.getText());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("cancel"))
+        {
+            this.notify("creditCardCancel");
+        } else {
+            try {
+                CreditCard card = new CreditCard(0.0,
+                        this.holderTextField.getText(),
+                        new Long(this.numberTextField.getText().replace(" ", "")),
+                        new Integer(this.securityCodeTextField.getText()),
+                        this.expirationMonthBox.getSelectedIndex(),
+                        this.expirationYearBox.getSelectedIndex());
+
+                this.notify("creditCardAction", card);
+            } catch (Exception exception) {
+                this.notify("creditCardError");
+            }
+        }
     }
 }

5ec1bf53ce66a2a69a91996e4d8bfbe94e4ecb5a
devinant <dino.opijac@gmail.com>
2014-05-22 23:21:11 +0200

    Added depositPerformed() and withdrawPerformed()

--- casino/controllers/TransactionController.java
+++ casino/controllers/TransactionController.java
@@ -1,6 +1,8 @@
 package casino.controllers;
 
 import casino.MainFrame;
+import casino.events.TransactionEvent;
+import casino.events.TransactionListener;
 import casino.views.DepositView;
 
 import java.awt.*;
@@ -9,12 +11,22 @@ import java.awt.*;
  * @author  Dino Opijac
  * @since   21/05/2014
  */
-public class TransactionController {
-    private DepositView view = new DepositView();
+public class TransactionController implements TransactionListener {
+    private DepositView depositView = new DepositView();
 
     public TransactionController() {
-        this.view.subscribe(this);
+        this.depositView.subscribe(this);
 
-        MainFrame.getInstance().add(this.view, BorderLayout.CENTER);
+        MainFrame.getInstance().add(this.depositView, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void depositPerformed(TransactionEvent e) {
+        System.out.println(String.format("Deposit performed: %s", e.getPayment()));
+    }
+
+    @Override
+    public void withdrawPerformed(TransactionEvent e) {
+        System.out.println(String.format("Withdraw performed: %s", e.getPayment()));
     }
 }

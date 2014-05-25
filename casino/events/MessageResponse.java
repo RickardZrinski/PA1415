76a4062aaeadb75cfe97473272897b958a03994e
devinant <dino.opijac@gmail.com>
2014-05-25 05:45:58 +0200

    Added MessageResponse interface

---
+++ casino/events/MessageResponse.java
@@ -0,0 +1,10 @@
+package casino.events;
+
+/**
+ * @author  Dino Opijac
+ * @since   18/05/2014
+ */
+public interface MessageResponse {
+    public void messageSent();
+    public void messageNotSent();
+}

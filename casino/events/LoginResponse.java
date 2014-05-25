315c392b010b1d4b9d9a4af42d6fa9e39b93c50b
devinant <dino.opijac@gmail.com>
2014-05-25 05:46:27 +0200

    Added comments

--- casino/events/LoginResponse.java
+++ casino/events/LoginResponse.java
@@ -1,5 +1,9 @@
 package casino.events;
 
+/**
+ * @author  Dino Opijac
+ * @since   18/05/2014
+ */
 public interface LoginResponse {
     public void loginSuccessful();
     public void loginUnsuccessful();

4ed535aedf85b9a08a0dbd38e780a8a21fba7b70
devinant <dino.opijac@gmail.com>
2014-05-25 05:42:54 +0200

    Added contracts according to specification

--- casino/controllers/GameController.java
+++ casino/controllers/GameController.java
@@ -1,21 +1,47 @@
 package casino.controllers;
 
-import casino.MainFrame;
+import casino.events.GameListener;
 import casino.views.GameView;
-import shared.game.GameSession;
 
 /**
  * @author  Dino Opijac
  * @since   21/05/2014
  */
-public class GameController {
-    private GameSession model;
+public class GameController implements GameListener {
     private GameView view;
 
     public GameController() {
         this.view = new GameView();
         this.view.subscribe(this);
+    }
 
-        MainFrame.getInstance().add(this.view);
+    @Override
+    public void selectGame(int index) {
+        System.out.println(String.format("GameController: Selected game index: %d", index));
+    }
+
+    @Override
+    public void bet(double amount) {
+        System.out.println(String.format("GameController: Bet %f", amount));
+    }
+
+    @Override
+    public void toss() {
+        System.out.println("GameController: Toss performed");
+    }
+
+    @Override
+    public void saveDie(int index) {
+        System.out.println(String.format("GameController: Save die index: %d", index));
+    }
+
+    @Override
+    public void end() {
+        System.out.println("GameController: end");
+    }
+
+    @Override
+    public void playAgain() {
+        System.out.println("GameController: playAgain");
     }
 }

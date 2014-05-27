783da7032552f1406c59eea8f1004bb303a379f9
Rickard Zrinski <ricco3@hotmail.com>
2014-05-27 13:05:38 +0200

    Add EditUserView/Controller

---
+++ administrator/controllers/EditUserController.java
@@ -0,0 +1,12 @@
+package administrator.controllers;
+
+/**
+ * Created by Rickard Zrinski on 2014-05-27.
+ */
+public class EditUserController
+{
+    public EditUserController()
+    {
+
+    }
+}
---
+++ administrator/views/EditUserView.java
@@ -0,0 +1,12 @@
+package administrator.views;
+
+/**
+ * Created by Rickard Zrinski on 2014-05-27.
+ */
+public class EditUserView
+{
+    public EditUserView()
+    {
+
+    }
+}

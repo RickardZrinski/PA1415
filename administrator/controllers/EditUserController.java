5bab73a7b02ceadb96a6887223b8e0a90ad0135b
Rickard Zrinski <ricco3@hotmail.com>
2014-05-27 14:09:51 +0200

    Add basic structure to EditUserView/Controller

--- administrator/controllers/EditUserController.java
+++ administrator/controllers/EditUserController.java
@@ -1,11 +1,44 @@
 package administrator.controllers;
 
+import administrator.GUI;
+import administrator.models.UsersModel;
+import administrator.views.EditUserView;
+import shared.users.User;
+
+import java.awt.event.ActionEvent;
+
 /**
  * Created by Rickard Zrinski on 2014-05-27.
  */
-public class EditUserController
+public class EditUserController extends Controller
 {
-    public EditUserController()
+    private EditUserView m_view;
+    private UsersModel m_usersModel;
+    private int m_userIndex;
+
+    public EditUserController(GUI gui, UsersModel usersModel, int userIndex)
+    {
+        super(gui);
+
+        m_usersModel = usersModel;
+        m_userIndex = userIndex;
+
+        showUserData();
+    }
+
+    private void showUserData()
+    {
+        m_view = new EditUserView();
+        m_view.setController(this);
+
+        User user = m_usersModel.getUser(m_userIndex);
+
+        this.getGui().addView(m_view, "EditGameView");
+        this.getGui().showView("EditGameView", "Administrator - Edit game");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
     {
 
     }
--- administrator/views/EditUserView.java
+++ administrator/views/EditUserView.java
@@ -1,12 +1,152 @@
 package administrator.views;
 
+import administrator.controllers.EditUserController;
+import administrator.utilities.gui.DefaultButton;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Rickard Zrinski on 2014-05-27.
  */
-public class EditUserView
+public class EditUserView extends View
 {
+    private EditUserController m_controller;
+    private JTextField m_userNameField;
+    private JTextField m_firstNameField;
+    private JTextField m_lastNameField;
+    private JPasswordField m_passwordField;
+    private DefaultButton m_saveBtn;
+
     public EditUserView()
     {
+        initialize();
+        configureView();
+        configureComponents();
+        addComponents();
+    }
 
+    private void initialize()
+    {
+        m_userNameField = new JTextField();
+        m_firstNameField = new JTextField();
+        m_lastNameField = new JTextField();
+        m_passwordField = new JPasswordField();
+        m_saveBtn = new DefaultButton("Save");
+    }
+
+    private void configureView()
+    {
+        this.setLayout(new GridBagLayout());
+    }
+
+    private void configureComponents()
+    {
+        m_userNameField.setColumns(10);
+        m_firstNameField.setColumns(10);
+        m_lastNameField.setColumns(10);
+        m_passwordField.setColumns(10);
+    }
+
+    private void addComponents()
+    {
+        // Configure the overall layout and add components
+        GridBagConstraints constraints;
+        JPanel cellPanel;
+
+        // The username label
+        constraints = new GridBagConstraints();
+        constraints.gridy = 0;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(new JLabel("Username"));
+        this.add(cellPanel, constraints);
+
+        // The username text field
+        constraints = new GridBagConstraints();
+        constraints.gridy = 1;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(m_userNameField);
+        this.add(cellPanel, constraints);
+
+        // The firstname label
+        constraints = new GridBagConstraints();
+        constraints.gridy = 2;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(new JLabel("Firstname"));
+        this.add(cellPanel, constraints);
+
+        // The firstname text field
+        constraints = new GridBagConstraints();
+        constraints.gridy = 3;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(m_firstNameField);
+        this.add(cellPanel, constraints);
+
+        // The lastname label
+        constraints = new GridBagConstraints();
+        constraints.gridy = 4;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(new JLabel("Lastname"));
+        this.add(cellPanel, constraints);
+
+        // The lastname text field
+        constraints = new GridBagConstraints();
+        constraints.gridy = 5;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(m_lastNameField);
+        this.add(cellPanel, constraints);
+
+        // The password label
+        constraints = new GridBagConstraints();
+        constraints.gridy = 6;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(new JLabel("Password"));
+        this.add(cellPanel, constraints);
+
+        // The password text field
+        constraints = new GridBagConstraints();
+        constraints.gridy = 7;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(m_passwordField);
+        this.add(cellPanel, constraints);
+
+        // The "Save" button
+        constraints = new GridBagConstraints();
+        constraints.gridy = 8;
+        constraints.gridx = 0;
+        constraints.anchor = GridBagConstraints.WEST;
+
+        cellPanel = new JPanel();
+        cellPanel.add(m_saveBtn);
+        this.add(cellPanel, constraints);
+    }
+
+    public void setController(EditUserController controller)
+    {
+        m_controller = controller;
     }
 }
